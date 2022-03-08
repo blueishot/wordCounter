d25fd1af10805f32924e68ba28a7b5eed77394e5
blueishot <fabricioteixeirahungria@gmail.com>
2022-03-08 18:20:13 -0300

    html, art

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var palavra = window.prompt(`Qual palavra você gostaria de saber o nome?`)
-window.alert(`Sua palavra é ${palavra}, e o número de letras é ${palavra.length}`)
-
-if (palavra.length > 10) {
-    window.alert(`Sua palavra é muito grande`)
-} else {
-    window.alert(`Sua palavra está de acordo`)
+function calcular() {
+    var input = window.document.getElementsByClassName('textInput');
+    var quantity = window.document.getElementsByClassName('characterCount');
+    var numbers = input.length
+    quantity.innerHTML = `Isso é um teste`
 }
